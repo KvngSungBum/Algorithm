327d0e3101bf16ff0ea3347bdfffd69bb7db9aef
KvngSungBum <51185205+KvngSungBum@users.noreply.github.com>
2021-05-11 16:37:37 +0900

    Newly Solved Algorithm

--- src/Algorithm/Page180.java
+++ src/Algorithm/Page180.java
@@ -3,12 +3,37 @@ package Algorithm;
 import java.util.*;
 
 public class Page180 {
-    public static void main(String[] args){
+        public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
+        String[] name_list = new String[n];
+        int[] grade_list = new int[n];
+        String str= "";
+        int grade = 0;
         for(int i=0;i<n;i++){
-            String input_str = sc.nextLine();
-            String[] list = input_str.split(" ");
+            str = sc.next();
+            grade = sc.nextInt();
+            name_list[i]= str;
+            grade_list[i] = grade;
+            sc.nextLine();
+        }
+
+        for(int i=0;i<grade_list.length;i++){
+            for(int j=0;j<grade_list.length-i-1;j++){
+                if(grade_list[j]>grade_list[j+1]){
+                    int temp = grade_list[j+1];
+                    grade_list[j+1] = grade_list[j];
+                    grade_list[j]=temp;
+
+                    String temp2 = name_list[j+1];
+                    name_list[j+1] = name_list[j];
+                    name_list[j]=temp2;
+                }
+            }
+        }
+
+        for(int i=0;i<name_list.length;i++){
+            System.out.print(name_list[i]+" ");
         }
     }
 }
